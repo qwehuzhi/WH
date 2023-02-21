acf227e96463a7dd32f09da062c45981bd3c0d27
huzhi <1780024678@qq.com>
2023-02-21 13:13:32 +0800

    修改console的pom，全局异常配置日志

--- wh/console/src/main/java/com/huzhi/console/controller/GlobalControllerAdvice.java
+++ wh/console/src/main/java/com/huzhi/console/controller/GlobalControllerAdvice.java
@@ -4,6 +4,9 @@ import com.huzhi.module.response.Response;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
+
+
 /**
  * 全局异常捕获
  *@RestControllerAdvice都是对Controller进行增强的，可以全局捕获spring mvc抛的异常。
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * ExceptionHandler 可以全局仅捕获一种异常，也可以全局捕获多种异常，从上到下 依次处理
  *
  */
+@Slf4j
 @RestControllerAdvice
 public class GlobalControllerAdvice {
     /**
@@ -21,6 +25,7 @@ public class GlobalControllerAdvice {
      */
     @ExceptionHandler(Exception.class)
     public Response handleStoreAuthException(Exception e) {
+        log.error(e.getMessage(),e);
         return new Response<>(4004,e.toString());
     }
 }
